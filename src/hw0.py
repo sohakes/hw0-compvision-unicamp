152674c137fe205c18105580c8a030ffaa291c28
Nathana Facion <nathanafacion@gmail.com>
2017-08-04 12:32:19 -0300

    add funcoes

--- src/hw0.py
+++ src/hw0.py
@@ -1,128 +1,129 @@
 import cv2
 import numpy as np
 
-#Question 2
-#Loads color image
-img = cv2.imread('input/p0-1-0.png')
+################  HW0  #####################
+# Nathana Facion                 RA:191079
+# Rafael Mariottini Tomazela     RA:192803 
+############################################
 
-#split into channels
-b,g,r = cv2.split(img)
+DEBUG = True
 
-#merge back but changint red and blue
-imgswaprb = cv2.merge((r, g, b))
-cv2.imshow('image', imgswaprb)
+def Questao2(r,g,b):
+    #merge back but changint red and blue
+    imgswaprb = cv2.merge((r, g, b))
 
-#Show image for debugging purpose
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    #show and save
+    if DEBUG == True : debug('image', imgswaprb)
+    cv2.imwrite('output/p0-2-a-0.png', imgswaprb)
 
-#save it
-cv2.imwrite('output/p0-2-a-0.png', imgswaprb)
+    #green channel img / show and save
+    if DEBUG == True : debug('image', g)
+    cv2.imwrite('output/p0-2-b-0.png', g)
 
-#green channel img
-#show and save
-cv2.imshow('image', g)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('output/p0-2-b-0.png', g)
+    #red channel img /show and save
+    if DEBUG == True : debug('image', r)
+    cv2.imwrite('output/p0-2-c-0.png', r)
 
-#red channel img
-#show and save
-cv2.imshow('image', r)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('output/p0-2-c-0.png', r)
+def Questao3(r,g,b):
+    answer = g
+    other = r
 
-#Question 3
-answer = g
-other = r
+    #now lets create A and B
+    A = answer
+    Bout = other.copy()
 
-#now lets create A and B
-A = answer
-Bout = other.copy()
+    (rows, cols) = answer.shape
 
-(rows, cols) = answer.shape
+    swidth = int(rows/2 - 50)
+    ewidth = swidth + 100
+    sheight = int(cols/2 - 50)
+    eheight = sheight + 100
 
-swidth = int(rows/2 - 50)
-ewidth = swidth + 100
-sheight = int(cols/2 - 50)
-eheight = sheight + 100
+    Bout[swidth:ewidth, sheight:eheight] = A[swidth:ewidth, sheight:eheight]
 
-Bout[swidth:ewidth, sheight:eheight] = A[swidth:ewidth, sheight:eheight]
+    if DEBUG == True : debug('Bout', Bout)
+    cv2.imwrite('output/p0-3-a-0.png', Bout)
 
-cv2.imshow('Bout', Bout)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('output/p0-3-a-0.png', Bout)
+    #Now recreate the original image with the correct channel
+    #Since I think the answer is the red one, gonna change that
 
-#Now recreate the original image with the correct channel
-#Since I think the answer is the red one, gonna change that
+    imgchangedred = cv2.merge((b, g, Bout))
+    if DEBUG == True : debug('imgchangedred', imgchangedred)
+    cv2.imwrite('output/p0-3-b-0.png', imgchangedred)
 
-imgchangedred = cv2.merge((b, g, Bout))
+    print("Min of green channel: %.2f" % g.min())
+    print("Max of green channel: %.2f" % g.max())
+    print("Mean of green channel: %.2f" % g.mean())
+    print("Std of green channel: %.2f" % g.std())
 
-cv2.imshow('imgchangedred', imgchangedred)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('output/p0-3-b-0.png', imgchangedred)
+def Questao4(r,g,b):
+    #normalize g
+    gmean = float(g.mean())
+    gstd = float(g.std())
 
-print("Min of green channel: %.2f" % g.min())
-print("Max of green channel: %.2f" % g.max())
-print("Mean of green channel: %.2f" % g.mean())
-print("Std of green channel: %.2f" % g.std())
+    ng = g.astype(float)
 
-#question 4
-#normalize g
-gmean = float(g.mean())
-gstd = float(g.std())
+    ng = ng - gmean
+    ng /= gstd
+    ng *= 10
+    ng += gmean
 
-ng = g.astype(float)
+    ngout = ng.astype(int)
+    if DEBUG == True : debug('ng', ngout)
+    cv2.imwrite('output/p0-4-b-0.png', ngout)
 
-ng = ng - gmean
-ng /= gstd
-ng *= 10
-ng += gmean
+    print("The colors of the image are smoother")
 
-ngout = ng.astype(int)
+    #roll the green channel
+    shiftedgreen = np.roll(g, 2, axis=1)
 
-cv2.imshow('ng', ngout)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('output/p0-4-b-0.png', ngout)
+    #subtract
+    resultsubshift = g - shiftedgreen
+    if resultsubshift.min() < 0:
+        resultsubshift += resultsubshift.min()
+ 
+    if DEBUG == True : debug('shiftedgreen',resultsubshift)
+    cv2.imwrite('output/p0-4-c-0.png', resultsubshift)
 
-print("The colors of the image are smoother")
+    print("A imagem subtraida tem principalmente as bordas, porque eh onde muda de cor.. acho")
 
-#roll the green channel
-shiftedgreen = np.roll(g, 2, axis=1)
 
-#subtract
-resultsubshift = g - shiftedgreen
-if resultsubshift.min() < 0:
-    resultsubshift += resultsubshift.min()
-cv2.imshow('shiftedgreen', resultsubshift)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('output/p0-4-c-0.png', resultsubshift)
+def Questao5(r,g,b):
+    #Question 5
+    #create a matrix of the same size by copying
+    gaussian_noise = g.copy()
+    cv2.randn(gaussian_noise, 0, 30);
 
-print("A imagem subtraida tem principalmente as bordas, porque eh onde muda de cor.. acho")
+    #add to the img
+    noisy_g = g + gaussian_noise
+    noisy_imgg = cv2.merge((b, noisy_g, r))
+    if DEBUG == True : debug('noisy_imgg',noisy_imgg)
+    cv2.imwrite('output/p0-5-a-0.png', noisy_imgg)
 
-#Question 5
-#create a matrix of the same size by copying
-gaussian_noise = g.copy()
-cv2.randn(gaussian_noise, 0, 30);
+    noisy_b = b + gaussian_noise
+    noisy_imgb = cv2.merge((noisy_b, g, r))
+    if DEBUG == True : debug('noisy_imgg',noisy_imgg)
+    cv2.imwrite('output/p0-5-b-0.png', noisy_imgb)
 
-#add to the img
-noisy_g = g + gaussian_noise
-noisy_imgg = cv2.merge((b, noisy_g, r))
-cv2.imshow('noisy_imgg', noisy_imgg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('output/p0-5-a-0.png', noisy_imgg)
 
-noisy_b = b + gaussian_noise
-noisy_imgb = cv2.merge((noisy_b, g, r))
-cv2.imshow('noisy_imgb', noisy_imgb)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('output/p0-5-b-0.png', noisy_imgb)
+def debug(name,img):
+    cv2.imshow(name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-print("substituir azul ficou melhor, n sei pq")
+def main():
+    #Loads color image
+    img = cv2.imread('input/p0-1-0.png')
+    
+    #split into channels
+    b,g,r = cv2.split(img)
+
+    Questao2(r,g,b)
+    Questao3(r,g,b)
+    Questao4(r,g,b)
+    Questao5(r,g,b)
+
+
+if __name__ == '__main__':
+   main()
+
